960a304333518f6f605eb208a8c7fd208915e303
ecourse <smkang@codenuri.co.kr>
2022-07-27 08:43:02 +0900

    Add files via upload

--- DAY2/1_Decorator5.cpp
+++ DAY2/1_Decorator5.cpp
@@ -20,6 +20,22 @@ public:
 
 int main()
 {
+	// 객체지향(C++)은 생성자/소멸자로 자원 관리하므로 편리합니다.
+	FileStream fs("a.txt");
+	fs.Write("Hello");
+
+	if (실패)
+		return 0; // 이순간 fs 의 소멸자가 fclose 수행하게 됩니다.
+
+
+	// C 언어는 개발자가 직접 자원을 관리해야 하므로 불편합니다.
+	/*
 	FILE* f = fopen("a.txt", "wt");
+	if (실패)
+	{
+		fclose(f);
+		return 0;
+	}
 	fclose(f);
+	*/
 }

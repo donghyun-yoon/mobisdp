2404e5373624aff247926c72232939026aa29920
ecourse <smkang@codenuri.co.kr>
2022-07-27 16:27:57 +0900

    Add files via upload

---
+++ DAY2/정리.cpp
@@ -0,0 +1,21 @@
+// 23개의 패턴은 용도에 따라 3가지로 분류합니다.(교재 마지막 페이지)
+
+// 생성 패턴 5개 : 
+// => Singleton,       Prototype, Abstract Factory, 
+//    Factory Method,  Builder (축구선수 캐릭터 만드는 예제)
+
+// 구조 패턴 7개 :
+// 간접층 사용 : Adapter, Proxy, Facade, Bridge
+// 재귀적 포함 : Composite, Decorator
+// 객체의 공유 : Flyweight
+
+// 행위 패턴 11개 : 
+// 변하는것의 분리 : strategy, state, template method
+//                observer, chain of responsibility
+//				  command, memento
+//				  iterator(반복자), visitor(방문자)
+//                mediator(중재자, 통보센터 예제)
+//				  iterpreter.  컴파일러 만뜰때 사용하는 패턴(지금은 사용안함)
+
+// 질문은 smkang @ codenuri.co.kr 
+

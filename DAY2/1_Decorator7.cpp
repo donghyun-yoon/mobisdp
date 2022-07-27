9517768b743af13d690a1c3209e99e70c653e07e
ecourse <smkang@codenuri.co.kr>
2022-07-27 09:08:45 +0900

    Add files via upload

--- DAY2/1_Decorator7.cpp
+++ DAY2/1_Decorator7.cpp
@@ -28,9 +28,25 @@ int main()
 {
 	// 파일을 쓸때 암호화 해서 쓰는 기능이 필요 합니다.
 	// 방법 1. 암호화를 먼저하고 파일에 쓰자. 
+	//	=> 그런데, 이런 작업을 자주 한다면 ??!!
 
+	// 방법 2. FileStream 안에 EncrpytWrite() 함수 추가
+	//  => NetworkStream 등 다른 Stream 클래스도 필요 하다.
+	//  => 요구 사항이 있을때마다 기존 클래스를 변경하는것은 좋지 않다.
+	//     OCP 위반
+
+	// 방법3. 기존 클래스의 수정없이 기능을 추가할수 있어야 한다.
+	//       Decorator 패턴 사용
 
 	FileStream fs("a.txt");
 	fs.Write("Hello");
+
+	EncryptDecorator ed(&fs); // fs뿐아니라 파이프, 네트워크 스트림도가능
+	ed.Write("Hello");	// 1. 데이타를 암호화 하고  <= 추가 기능
+						// 2. fs.Write() 로 쓰기  <= 원래 기능
+
+	ZipDecorator zd(&ed);
+	zd.Write("Hello");  // 1. 데이타를 압축하고
+					    // 2. ed.Write()로 전달
 
 }
